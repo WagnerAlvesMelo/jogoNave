9028fda6b7aabf4868d12f7b3a2efa6fff859044
WagnerAlvesMelo <walves13@outlook.com>
2019-08-17 22:25:06 -0300

    Declaração do objeto Sprite

---
+++ assets/js/object.js
@@ -0,0 +1,10 @@
+var sprite = function(sourceX,sourceY,width,heigth,x,y){
+    this.sourceX = sourceX;
+    this.sourceY = sourceY;
+    this.width = width;
+    this.heigth = heigth;
+    this.x=x;
+    this.y=y;
+    this.vx=0;
+    this.vy=0;
+}
